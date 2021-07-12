bf835d51ed18243a91f5e7e396b9cb18d7329e6b
ValeriyaS-code <85139016+ValeriyaS-code@users.noreply.github.com>
2021-07-11 22:06:53 -0400

    password generator completed

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var char = ['!','@','#','$','%','^','&','*','_','-','+','='];
 var randomPassword = [];
+var userPassword = [];
 
 var generatePassword = function() {
   window.alert("Please select which criteria you would like to use in your password");
@@ -32,20 +33,26 @@ var generatePassword = function() {
     randomPassword = randomPassword.concat(char);
   }
 
+  for (var i = 0; i < passwordLength; i++) {
+      
+    var newPassword = randomPassword[Math.floor(Math.random() * randomPassword.length)]
+      userPassword += newPassword;
+    }
+    return userPassword;
 };
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-//function writePassword() {
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-//passwordText.value = password;
+passwordText.value = password;
 
-//}
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
+
